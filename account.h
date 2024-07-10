c3420c32ac7c170e241dcf7912f40ef649660437
Zumarongi <2769486846@qq.com>
2024-07-10 19:46:37 +0800

    Add several files

---
+++ account.h
@@ -0,0 +1,45 @@
+#ifndef _ACCOUNT_H_
+#define _ACCOUNT_H_
+
+#include<string>
+#include<vector>
+#include<map>
+
+enum taskPriority { LOW = 0, MID, HIGH };
+
+std::map<int, std::string> mpCtg;
+
+class Task
+{
+    static int IdCounter;
+    int taskId;
+
+    std::string taskName;
+    time_t stTime, edTime, rmTime;  // start, end, reminder
+    std::string taskLoc;            // location
+    taskPriority taskPrio;
+    int taskCtg;                    //catagory
+    std::string taskNote;
+
+public:
+    Task(std::string taskname, time_t st_time, time_t ed_time, time_t rm_time,
+         std::string taskloc, taskPriority taskprio, int taskctg,
+         std::string tasknote);
+    bool saveToFile();
+    void printInfo();
+    
+};
+
+class Account
+{
+
+public:
+    std::string Username;
+    std::string encryptedPass;
+    std::vector<Task *> taskList;
+    bool showHelp;
+    bool doneAndDel;
+    
+};
+
+#endif //_ACCOUNT_H_
