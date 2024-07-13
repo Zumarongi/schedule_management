2295f60cd549a056528c6ba6a1fca83effdc2cce
Zumarongi <2769486846@qq.com>
2024-07-13 18:05:09 +0800

    build

--- timeformat.cpp
+++ timeformat.cpp
@@ -2,15 +2,15 @@
 
 time_t timeformat::toTime_t(QDateTime qDateTime)
 {
-
+    return 0;
 }
 
 time_t timeformat::toTime_t(int year, int month, int day, int hour, int minute)
 {
-
+    return 0;
 }
 
 time_t timeformat::toTime_tSegment(int hour, int minute)
 {
-
+    return 0;
 }
